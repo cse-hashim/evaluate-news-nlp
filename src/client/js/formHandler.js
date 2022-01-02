ea6240ddbb67bc075191530211125169801c6f14
Hashim Hossam Mahmoud Mohammed <cse-hashim@users.noreply.github.com>
2022-01-02 07:46:46 +0200

    fix: nameChecker
    In the file nameChecker.js we are required to implement our own custom validations. These validations need to be invoked before API calls in our file formHandler.js. Please implement these validations and add before API calls.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -14,19 +14,19 @@ const handleSubmit = async (event) => {
 
   document.getElementById('results').innerHTML = ''
   const target = document.getElementById('name').value
-  const isUrl = (e) => {
-    let u;
-    try {
-      u = new URL(e);
-    } catch (_) {
-      return false;
-    }
-    return u.protocol === "http:" || u.protocol === "https:";
-  }
-  const type = isUrl(target) ? 'url' : 'txt';
-  Client.checkForName(target)
-  console.log("::: Form Submitted :::")
+  // const isUrl = (e) => {
+  //   let u;
+  //   try {
+  //     u = new URL(e);
+  //   } catch (_) {
+  //     return false;
+  //   }
+  //   return u.protocol === "http:" || u.protocol === "https:";
+  // }
+  // const type = isUrl(target) ? 'url' : 'txt';
+  const type = Client.checkForName(target) ? 'url' : 'txt';
   return new Promise(async (resolve, _) => {
+    console.log("::: Form Submitted... :::")
     resolve(await sendPost({ target, type }));
   })
     .then(async (res) => {
@@ -48,6 +48,7 @@ const handleSubmit = async (event) => {
       if (res.status.msg !== 'OK') document.getElementById('results').innerHTML = res.status.msg
       document.getElementById('name').value = ''
       document.getElementById('name').style.textDecoration = 'none'
+      console.log("::: Server Responded :::",res)
     })
 }
 
--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,16 +1,18 @@
-function checkForName(inputText) {
-    console.log("::: Running checkForName :::", inputText);
-    let names = [
-        "Picard",
-        "Janeway",
-        "Kirk",
-        "Archer",
-        "Georgiou"
-    ]
-
-    if(names.includes(inputText)) {
-        alert("Welcome, Captain!")
+function checkForName(e) {
+    console.log("::: checking for empty input :::");
+    if(e===''){console.log("::: input is empty :::");return false};
+    console.log("::: checking input for being URL :::", e);
+    let u;
+    try {
+        u = new URL(e);
+    } catch (err) {
+        console.log("::: you  didn't enter a valid URL :::",err);
+        console.log("::: trying to use it as text ... :::");
+        return false;
     }
+    console.log("::: URL is valid :::");
+    return u.protocol === "http:" || u.protocol === "https:";
 }
+
 
 export { checkForName }
